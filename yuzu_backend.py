68c4f67c67c2032bb8f537b90e32dbc8f2b0c783
mohit086 <155368106+mohit086@users.noreply.github.com>
2024-02-25 13:16:39 +0530

    added a config file to change theme

--- yuzu_backend.py
+++ yuzu_backend.py
@@ -1,6 +1,11 @@
-import yt_dlp
+import tempfile
+import os
 
-def download_file(params):
-
-    with yt_dlp.YoutubeDL() as ydl:
-        ydl.download(params['url'])
+def extract_file(params):
+    temp_dir = tempfile.mkdtemp()
+    url = params['url']
+    command = f'yt-dlp {url} -o {temp_dir}/%(title)s.%(ext)s'
+    os.system(command)
+    extracted_file = os.path.join(temp_dir, os.listdir(temp_dir)[0])
+    filename = os.path.basename(extracted_file)
+    return extracted_file, filename
--- yuzu_frontend.py
+++ yuzu_frontend.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import yuzu_backend
-import time
 
 def main():
     st.set_page_config(page_title = 'Yuzu',page_icon = ':lemon:', menu_items = {'Get help':'https://github.com/mohit086/yuzu'})
@@ -13,19 +12,20 @@ def main():
     ''', unsafe_allow_html=True)
 
     st.title('Yuzu')
-    url_column, download_column = st.columns([4,1])
+    url_column, extract_column = st.columns([4,1])
+    audio_only_column, playlist_column = st.columns(2)
 
     url = url_column.text_input('URL',placeholder = 'Enter a valid video / audio URL',label_visibility='collapsed')
-    download_button = download_column.button('Download',use_container_width=True)
-    download_params = {'url':url, 'donwload_button':download_button}
+    extract_button = extract_column.button('Extract',use_container_width=True)
+    audio_only = audio_only_column.checkbox('Audio only')
+    playlist = playlist_column.checkbox('Playlist')
+    download_params = {'url':url, 'extract_button':extract_button, 'audio_only':audio_only}
 
-    if download_button:
-        with st.spinner("Downloading ..."):
-            yuzu_backend.download_file(download_params)
-        success_placeholder = st.empty()
-        success_placeholder.success("Downloaded successfully")
-        time.sleep(2)
-        success_placeholder.empty()
-
+    if extract_button:
+        with st.spinner("Extracting ..."):
+            extracted_file, filename = yuzu_backend.extract_file(download_params)
+        with open(extracted_file, "rb") as f:
+            st.download_button(label="Download", data = f, file_name = filename)
+        
 if __name__ == '__main__':
     main()
